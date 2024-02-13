662de07ecf508fa0bfafd3b86bbba5f381e32ec9
Nasweha <nasweha@gmail.com>
2024-02-13 05:31:22 +0530

    javascript basic

---
+++ const.js
@@ -0,0 +1,30 @@
+// const y = 25;
+// console.log(y)
+// y =30 // erroy re assigning not possible
+// console.log(y)
+
+
+//  var x  =30;
+//  console.log(y);
+//  x=90;
+//  console.log(y)
+
+
+// console.log('-------------')
+
+// console.log(a)
+// var a = 24 //hoisting possible on var
+
+// console.log(b) // hoisting not possible 
+// const b = 24
+
+
+const x = 5;
+console.log(`my flat number is ${x}`);
+
+const company = 'luminar';
+const place = 'kakanad';
+console.log( `my company ${company}  is locate in ${place}`);
+
+
+
---
+++ prog1.js
@@ -0,0 +1,27 @@
+console.log(typeof('hello world'));
+console.log(typeof(false));
+console.log(typeof(27));
+console.log(typeof(12.7));
+
+
+
+
+console.log('---------------');
+console.log('CONCATANATION')
+console.log('2'+3);
+console.log('nasiha'+'shirin');
+console.log(2+3);
+
+console.log('----------------')
+console.log('variable')
+x= '24'
+console.log(x);
+
+
+
+
+
+
+
+
+
